262470ffb82892b53c64ce799c2e29e0f845c97d
andrius4669 <andrius4669@gmail.com>
2014-10-26 21:34:37 +0200

    include steelribs in maprotation, fix geoip check on ubuntu 14.04

--- src/check_geoip.sh
+++ src/check_geoip.sh
@@ -9,7 +9,7 @@ cat > check_geoip.cpp << EOF
 int main() { GeoIP *gi = NULL; GeoIP_delete(gi); return 0; }
 EOF
 
-$CXX check_geoip.cpp -o check_geoip 2>/dev/null
+$CXX check_geoip.cpp -o check_geoip "-lGeoIP" 2>/dev/null
 if [ $? -eq 0 ]; then printf "1"; rm check_geoip; fi
 echo ''
 rm check_geoip.cpp
